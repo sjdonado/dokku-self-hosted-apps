4169c50e74d9f0f8d9ff5701e9289db041752ef3
Juan Rodriguez <sjdonado@uninorte.edu.co>
2023-04-12 07:13:27 +0200

    feat: squid.conf

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,7 @@
 # OpenVPN
 git remote add dokku-openvpn dokku@sjdonado.de:openvpn
 git subtree push --prefix openvpn dokku-openvpn master
+
+# squid
+git remote add dokku-squid dokku@sjdonado.de:squid-proxy-server
+git subtree push --prefix squid-proxy-server dokku-squid master
